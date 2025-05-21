67506c48f894cb740be1fa817c24986dbcea11ba
Iamgovinda <Jamkatelgovinda1@gmail.com>
2025-05-21 14:55:02 +0545

    add: adds solution for two sum problem

---
+++ problems/two_sum.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/two-sum/description/
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i_index, i in enumerate(nums):
+            diff = target - i
+            try:
+                j_index = nums.index(diff)
+                if j_index == i_index:
+                    j_index = nums.index(diff, j_index + 1)
+            except ValueError:
+                continue
+            return [i_index, j_index]
