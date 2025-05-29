5af831b6a189f976dfb83a2bdc4e4d2dbdc60c14
Iamgovinda <Jamkatelgovinda1@gmail.com>
2025-05-29 15:17:44 +0545

    add: adds odd even linked list optimized

---
+++ problems/odd_even_linked_list_optimized.py
@@ -0,0 +1,27 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not head or not head.next:
+            return head
+
+        odd = head
+        even = head.next
+        even_head = even  # Store head of even list
+
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+
+        odd.next = even_head  # Append even list after odd list
+        return head
